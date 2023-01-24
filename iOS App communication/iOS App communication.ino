686ed5614c15d1011bb0866f618de24e68452119
Kimpalele <kim99007@gmail.com>
2023-01-24 19:40:53 +0100

    Update iOS App communication.ino

--- iOS App communication/iOS App communication.ino
+++ iOS App communication/iOS App communication.ino
@@ -5,7 +5,7 @@ void setup() {
   Serial.begin(9600);
 
   while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
+    ; // wait for serial port to connect.
   }
 
   //Configure the arduino pins to output
