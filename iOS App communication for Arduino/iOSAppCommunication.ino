25de94a6d37eb28fa4b0e642612660af007c4d9c
Kim Jogholt <kim99007@gmail.com>
2023-06-09 15:11:25 +0200

    Update iOSAppCommunication.ino
    Implemented MixDrink()

--- iOS App communication for Arduino/iOSAppCommunication.ino
+++ iOS App communication for Arduino/iOSAppCommunication.ino
@@ -1,3 +1,7 @@
+//2000 mS ≈ 1 cL
+//relayPin 1 = arduinoPin 2
+//. . . . .
+//relayPin 6 = arduinoPin 7
 
 #include "functions.h"
 
@@ -5,24 +9,23 @@ void setup() {
   Serial.begin(9600);
 
   while (!Serial) {
-    ; // wait for serial port to connect.
+    ; // wait for serial port to connect. Needed for native USB port only
   }
 
-  //Configure the arduino pins to output
-  pinMode(pump1, OUTPUT); 
-  pinMode(pump2, OUTPUT); 
-  pinMode(pump3, OUTPUT);
-  pinMode(pump4, OUTPUT);
-  pinMode(pump5, OUTPUT);
-  pinMode(pump6, OUTPUT);
+  pinMode(vodka, OUTPUT); 
+  pinMode(cranberry, OUTPUT); 
+  pinMode(redbull, OUTPUT);
+  pinMode(sprite, OUTPUT);
+  pinMode(gin, OUTPUT);
+  pinMode(juice, OUTPUT);
 
   //Make sure all pumps are off, current relay board is "high true".  
-  digitalWrite(pump1,LOW);
-  digitalWrite(pump2,LOW);
-  digitalWrite(pump3,LOW);
-  digitalWrite(pump4,LOW);
-  digitalWrite(pump5,LOW);
-  digitalWrite(pump6,LOW);
+  digitalWrite(vodka,LOW);
+  digitalWrite(cranberry,LOW);
+  digitalWrite(redbull,LOW);
+  digitalWrite(sprite,LOW);
+  digitalWrite(gin,LOW);
+  digitalWrite(juice,LOW);
 
 }
 
@@ -30,48 +33,59 @@ void loop() { // run over and over
   
   if(Serial.available()){ //Checks if we got any input
 
-      //Reads the received string and adds it into our string to be compared
-      String drink = Serial.readString(); 
+    //Reads the received string and adds it into our string to be compared
+    String drink = Serial.readString(); 
 
-      //Had to remove trailing newline for the comparison to work correctly
-      drink.trim();
-      
-      if (drink == "Cranberry"){
-        VodkaCranberry();
-        Serial.println(drink);
-      } else if (drink == "Redbull"){
-        VodkaRedbull();
-        Serial.println(drink);
-      } else if (drink == "Screwdriver"){
-        Screwdriver();
-      } else if (drink == "Pump1"){
-        prepareSpecific(pump1);
-        Serial.println("Done");
-      } else if (drink == "Pump2"){
-        prepareSpecific(pump2);
-        Serial.println("Done");
-      } else if (drink == "Pump3"){
-        prepareSpecific(pump3);
-        Serial.println("Done");
-      } else if (drink == "Pump4"){
-        prepareSpecific(pump4);
-        Serial.println("Done");
-      } else if (drink == "Pump5"){
-        prepareSpecific(pump5);
-        Serial.println("Done");
-      } else if (drink == "Pump6"){
-        prepareSpecific(pump6);
-        Serial.println("Done");
-      } else if (drink == "prepareAll") {
-        prepareAll();
-        Serial.println("Done");
-      } else if (drink == "cleanAll") {
-        Cleaning();
-        Serial.println("Done");
-      } else {
-        Serial.println("Unrecognizeable string ! ! !");
-      }
-      
+    //We have to remove the trailing newline for the comparison to work
+    //We could add \n to every string comparison but this is better
+    drink.trim();
+    
+    if (drink == "vodkacranberry6"){
+      MixDrink(vodka, cranberry, six);
+    } else if (drink == "redbull6"){
+      MixDrink(vodka, redbull, six);
+    } else if (drink == "screwdriver6"){
+      MixDrink(vodka, juice, six);
+    } else if (drink == "ginsprite6"){
+      MixDrink(gin, sprite, six);
+    } else if (drink == "vodkasprite6"){
+      MixDrink(vodka, sprite, six);
+    } else if (drink == "gincranberry6"){
+      MixDrink(gin, cranberry, six);
+    } else if (drink == "vodkacranberry8"){
+      MixDrink(vodka, cranberry, eight);
+    } else if (drink == "redbull8"){
+      MixDrink(vodka, redbull, eight);
+    } else if (drink == "screwdriver8"){
+      MixDrink(vodka, juice, eight);
+    } else if (drink == "ginsprite8"){
+      MixDrink(gin, sprite, eight);
+    } else if (drink == "vodkasprite8"){
+      MixDrink(vodka, sprite, eight);
+    } else if (drink == "gincranberry8"){
+      MixDrink(gin, cranberry, eight);
+    } else if (drink == "ginjuice6") {
+      MixDrink(gin, juice, six);
+    } else if (drink == "ginjuice8") {
+      MixDrink(gin, juice, eight);
+    } else if (drink == "1") {
+      CleanSpecific(vodka);
+    } else if (drink == "2") {
+      CleanSpecific(cranberry);
+    } else if (drink == "3") {
+      CleanSpecific(redbull);
+    } else if (drink == "4") {
+      CleanSpecific(sprite);
+    } else if (drink == "5") {
+      CleanSpecific(gin);
+    } else if (drink == "6") {
+      CleanSpecific(juice);
+    } else if (drink == "10") {
+      CleanAll();
+    } else {
+      Serial.println("Unrecognizeable string ! ! !");
+    }
+    
     //Some trailing whitespace are interfering with some of the comparisons
     //So we flush the serial port to get rid of it
     Serial.flush();      
